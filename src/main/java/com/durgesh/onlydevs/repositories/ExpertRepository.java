f1e90a67de357da7ff4366b18fd3336d55bd3b0b
Durgesh Pandey <durgeshpg3344@gmail.com>
2024-04-21 22:35:55 +0530

    Configuration for Deployment

--- src/main/java/com/durgesh/onlydevs/repositories/ExpertRepository.java
+++ src/main/java/com/durgesh/onlydevs/repositories/ExpertRepository.java
@@ -1,9 +1,10 @@
 package com.durgesh.onlydevs.repositories;
 
 import com.durgesh.onlydevs.entities.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExpertRepository extends CrudRepository<Expert, Integer> {
+public interface ExpertRepository extends JpaRepository<Expert, Integer> {
 }
